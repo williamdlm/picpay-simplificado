9adc43697043842e4703d6fc47728d2f8058bb70
William Mota <william.mota.ext@spms.min-saude.pt>
2024-02-19 18:51:35 -0300

    feat: Create User entity

---
+++ src/main/java/com/github/williamdlm/picpaysimplificado/domain/user/User.java
@@ -0,0 +1,27 @@
+package com.github.williamdlm.picpaysimplificado.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Entity(name = "users")
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true)
+    private String document;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private BigDecimal balance;
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+}
---
+++ src/main/java/com/github/williamdlm/picpaysimplificado/domain/user/UserType.java
@@ -0,0 +1,6 @@
+package com.github.williamdlm.picpaysimplificado.domain.user;
+
+public enum UserType {
+    COMMON,
+    MERCHANT
+}
